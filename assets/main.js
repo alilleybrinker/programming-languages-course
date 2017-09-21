df0f3bc5fb754fe1489664b1eb1783cb47549ea6
Andrew Brinker <me@andrewbrinker.com>
2017-09-20 20:52:23 -0700

    Added reading

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,26 @@
-// Set an event on header link clicks.
-$("h2 a[href^='#']").on("click", function (e) {
+let toggle = function (e, time) {
     e.preventDefault();
 
-    // When the button is clicked, toggle the collapsed state of the
-    // associated content.
     $(e.target)
         .toggleClass("collapsed")
         .parent()
         .next()
-        .toggle(150);
+        .toggle(time);
+};
+
+$("h2 a[href^='#']").on("click", function (e) {
+    toggle(e, 150);
 });
+
+let h3_links = $("#reading h3 a[href^='#']");
+
+h3_links.on("click", function (e) {
+    toggle(e, 150);
+});
+
+$(h3_links)
+    .toggleClass("collapsed")
+    .parent()
+    .next()
+    .toggle();
+
